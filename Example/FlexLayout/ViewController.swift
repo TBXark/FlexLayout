f66ca0503e8c021ec559dbe87fa8110b0d2d0df0
TBXark <tbxark@outlook.com>
2020-06-04 13:35:42 +0800

    Update demo

--- Example/FlexLayout/ViewController.swift
+++ Example/FlexLayout/ViewController.swift
@@ -48,79 +48,96 @@ class DemoView: UIView {
 }
 
 class ViewController: UIViewController {
-
+    
     let scrollView = UIScrollView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        scrollView.frame = view.bounds
-        let width = view.bounds.width
-        var maxY = CGFloat(80)
         
+        let demo1 = DemoView()
+        let demo2 = DemoView()
+        let demo3 = DemoView()
         
+        // Layout scrollView
         do {
-            let demo = DemoView(frame: CGRect(x: 0, y: maxY, width: width, height: 100))
+            scrollView.frame = view.bounds
+            
             FlexLayout.layout(size: [
-                .fixed(view: demo.red, value: 20),
-                .grow(view: demo.blue, scale: 1),
-                .grow(view: demo.green, scale: 2)
-            ], direction: .horizontal, align: .start, start: 0, end: width, space: .fixed(value: 10))
+                .space(.fixed(value: 80)),
+                .fixed(view: demo1, value: 100),
+                .fixed(view: demo2, value: 100),
+                .fixed(view: demo3, value: 400)
+            ], direction: .vertical, align: .start, start: 0, end: CGFloat.infinity, space: .fixed(value: 20))
             
             FlexLayout.layout(cross: [
-                .fixed(view: demo.red, value: 20, offset: 0, align: .start),
-                .fixed(view: demo.blue, value: 30, offset: -10, align: .end),
-                .stretch(view: demo.green, margin: .init(top: 10, left: 0, bottom: 10, right: 0))
-            ], direction: .horizontal, align: .start, start: 0, end: 100)
-            maxY = demo.frame.maxY + 20
-            scrollView.addSubview(demo)
+                .stretch(view: demo1),
+                .stretch(view: demo2),
+                .stretch(view: demo3, margin: .init(top: 0, left: 10, bottom: 0, right: 10))
+            ], direction: .vertical, end: scrollView.bounds.width)
+            
+            
+            scrollView.alwaysBounceVertical = true
+            scrollView.contentSize = CGSize(width: view.bounds.width, height: demo3.frame.maxY)
+            
+            view.addSubview(scrollView)
         }
         
         
+        // Layout demo1
         do {
-            let demo = DemoView(frame: CGRect(x: 0, y: maxY, width: width, height: 100))
             FlexLayout.layout(size: [
-                .fixed(view: demo.red, value: 60),
-                .fixed(view: demo.blue, value: 60),
-                .fixed(view: demo.green, value: 60)
-            ], direction: .horizontal, align: .start, start: 0, end: width, space: .grow(scale: 1))
+                .fixed(view: demo1.red, value: 20),
+                .grow(view: demo1.blue, scale: 1),
+                .grow(view: demo1.green, scale: 2)
+            ], direction: .horizontal, align: .start, start: 0, end: demo1.bounds.width, space: .fixed(value: 10))
             
             FlexLayout.layout(cross: [
-                .stretch(view: demo.red, margin: .init(top: 10, left: 0, bottom: 10, right: 0)),
-                .stretch(view: demo.blue, margin: .init(top: 10, left: 0, bottom: 10, right: 0)),
-                .stretch(view: demo.green, margin: .init(top: 10, left: 0, bottom: 10, right: 0))
-            ], direction: .horizontal, align: .start, start: 0, end: 100)
-            
-            maxY = demo.frame.maxY + 20
-            scrollView.addSubview(demo)
+                .fixed(view: demo1.red, value: 20, offset: 0, align: .start),
+                .fixed(view: demo1.blue, value: 30, offset: -10, align: .end),
+                .stretch(view: demo1.green, margin: .init(top: 10, left: 0, bottom: 10, right: 0))
+            ], direction: .horizontal, align: .start, start: 0, end:  demo2.bounds.height)
+            scrollView.addSubview(demo1)
         }
         
+        // Layout demo2
         do {
-            let demo = DemoView(frame: CGRect(x: 0, y: maxY, width: width, height: 400))
+            FlexLayout.layout(size: [
+                .fixed(view: demo2.red, value: 60),
+                .fixed(view: demo2.blue, value: 60),
+                .fixed(view: demo2.green, value: 60)
+            ], direction: .horizontal, align: .start, start: 0, end: demo2.bounds.width, space: .grow(scale: 1))
+            
+            FlexLayout.layout(cross: [
+                .stretch(view: demo2.red, margin: .init(top: 10, left: 0, bottom: 10, right: 0)),
+                .stretch(view: demo2.blue, margin: .init(top: 10, left: 0, bottom: 10, right: 0)),
+                .stretch(view: demo2.green, margin: .init(top: 10, left: 0, bottom: 10, right: 0))
+            ], direction: .horizontal, align: .start, start: 0, end: demo2.bounds.height)
+            scrollView.addSubview(demo2)
+        }
+        
+        // Layout demo3
+        do {
             FlexLayout.layout(size: [
                 .space(.fixed(value: 0)),
-                .fixed(view: demo.red, value: 60),
-                .fixed(view: demo.blue, value: 60),
-                .fixed(view: demo.green, value: 60),
+                .fixed(view: demo3.red, value: 60),
+                .fixed(view: demo3.blue, value: 60),
+                .fixed(view: demo3.green, value: 60),
                 .space(.fixed(value: 0)),
-            ], direction: .vertical, align: .start, start: 0, end: 400, space: .grow(scale: 1))
+            ], direction: .vertical, align: .start, start: 0, end: demo3.bounds.height, space: .grow(scale: 1))
             
             FlexLayout.layout(cross: [
-                .fixed(view: demo.red, value: 100, offset: 0, align: .start),
-                .fixed(view: demo.blue, value: 100, offset: 0, align: .center),
-                .fixed(view: demo.green, value: 100, offset: 0, align: .end)
-            ], direction: .vertical, align: .center, start: 0, end: width)
-            
-            maxY = demo.frame.maxY + 20
-            scrollView.addSubview(demo)
+                .fixed(view: demo3.red, value: 100, offset: 0, align: .start),
+                .fixed(view: demo3.blue, value: 100, offset: 0, align: .center),
+                .fixed(view: demo3.green, value: 100, offset: 0, align: .end)
+            ], direction: .vertical, align: .center, start: 0, end: demo3.bounds.width)
+            scrollView.addSubview(demo3)
         }
         
-        scrollView.contentSize = CGSize(width: width, height: maxY)
-
-        view.addSubview(scrollView)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
 }
 
